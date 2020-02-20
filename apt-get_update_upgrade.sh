bf8e2f8824f7e32a07d06faaab1f97d629181ee3
Juhani Kauppi <jkauppi@student.hive.fi>
2020-02-20 10:46:15 +0200

    Instruction updated

---
+++ apt-get_update_upgrade.sh
@@ -0,0 +1,4 @@
+date >> /var/log/update_script.log 2>&1
+apt-get update -y >> /var/log/update_script.log 2>&1
+date >> /var/log/update_script.log 2>&1
+apt-get upgrade -y >> /var/log/update_script.log 2>&1
